aabd345103cc0ccdeb6ffc9fa090a22e560f4f03
Alex Carol <acarolcampre@adobe.com>
2024-07-19 07:42:30 +0200

    Media gallery + cards import

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -58,11 +58,56 @@ function tabs(main, document) {
   });
 }
 
+function mediaGallery(main, document) {
+  main.querySelectorAll('.media-gallery').forEach((container) => {
+    const mgCards = [...container.querySelectorAll('.mg__card')];
+    const cells = mgCards.map((card) => {
+      const wrapper = card.querySelector('.mg-card__wrapper');
+      const href = wrapper.getAttribute('data-href');
+
+      const link = document.createElement('a');
+      link.setAttribute('href', href);
+      link.textContent = href;
+
+      card.append(link);
+
+      return [card];
+    });
+
+    const variants = [];
+    if (mgCards.length > 2 && mgCards[0].getAttribute('attr-width') === '2' && mgCards[1].getAttribute('attr-width') === '1') {
+      variants.push('alternate-2-1');
+    }
+
+    container.replaceWith(WebImporter.Blocks.createBlock(document, {
+      name: 'media-gallery',
+      cells,
+      variants,
+    }));
+  });
+}
+
+function cards(main, document) {
+  main.querySelectorAll('.card-container-v3').forEach((container) => {
+    const cells = [...container.querySelectorAll(':scope > ul > li > article')].map((card) => [card]);
+    if (!cells) {
+      throw new Error('No cards found', container.outerHTML);
+    }
+
+    container.replaceWith(WebImporter.Blocks.createBlock(document, {
+      name: 'cards',
+      cells,
+    }));
+  });
+}
+
 function transformers(main, document) {
   videos(main, document);
   calciteButton(main, document);
   storyteller(main, document);
   tabs(main, document);
+  mediaGallery(main, document);
+  cards(main, document);
 }
 
 export default {
