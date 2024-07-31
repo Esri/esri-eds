93dfdfe4183378de31f9f050a77e0a2f5740c115
Alex Carol <acarolcampre@adobe.com>
2024-07-31 08:16:31 +0200

    WIP

--- tools/importer/create_svg.js
+++ tools/importer/create_svg.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const csv = require('csv-parser');
 
 const csvFilePath = path.join(__dirname, 'import-report.csv');
