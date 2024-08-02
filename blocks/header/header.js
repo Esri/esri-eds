550757aba4fa3cbc4128ce6c2eb49a10ea5ed151
Alex Carol <acarolcampre@adobe.com>
2024-08-02 18:17:12 +0200

    Fix json reference

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -5,7 +5,7 @@ import { loadCSS, loadScript } from '../../scripts/aem.js';
  * @param {Element} block The header block element
  */
 export default async function decorate() {
-  window.gnav_jsonPath = '2022-nav-config.25.json';
+  window.gnav_jsonPath = '/2022-nav-config.25.json';
   loadScript('https://webapps-cdn.esri.com/CDN/components/global-nav/js/gn.js');
   loadCSS('https://webapps-cdn.esri.com/CDN/components/global-nav/css/gn.css');
 }
