02fedfc53ae3e7be458fd82cb1e50638eccec8fd
Tim White <tim_white@esri.com>
2024-08-02 08:20:33 -0700

    #43 feat: add Launch dev script (#58)
    * feat: add Launch dev script
    
    * feat: move Launch to delayed.js
    
    * fix: remove console.log
    
    * fix: update to append to head
    
    * feat: update to use loadScript
    
    * feat: update loadscript
    
    * feat: update loadScript
    
    * feat: fix loader

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,10 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { sampleRUM, loadScript } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// Launch script
+loadScript('https://assets.adobedtm.com/2d251f50426c/e52f833be42a/launch-bdb68bbb4cf5-development.min.js');
