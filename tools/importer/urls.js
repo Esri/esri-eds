1c38210c20df1d8f8b1cdf9fc968429e9a302408
Alex Carol <acarolcampre@adobe.com>
2024-08-07 10:30:18 +0200

    Update url.js to include calcite_mode

--- tools/importer/urls.js
+++ tools/importer/urls.js
@@ -1,47 +1,182 @@
 export default [
-  'https://www.esri.com/en-us/about/about-esri/americas',
-  'https://www.esri.com/en-us/about/about-esri/asia-pacific',
-  'https://www.esri.com/en-us/about/about-esri/company',
-  'https://www.esri.com/en-us/about/about-esri/europe/overview',
-  'https://www.esri.com/en-us/about/about-esri/locations',
-  'https://www.esri.com/en-us/about/about-esri/mea-central-asia',
-  'https://www.esri.com/en-us/about/about-esri/overview',
-  'https://www.esri.com/en-us/about/about-esri/technology',
-  'https://www.esri.com/en-us/capabilities/3d-gis/overview',
-  'https://www.esri.com/en-us/capabilities/3d-gis/resources',
-  'https://www.esri.com/en-us/capabilities/data-management',
-  'https://www.esri.com/en-us/capabilities/field-operations/get-started',
-  'https://www.esri.com/en-us/capabilities/field-operations/overview',
-  'https://www.esri.com/en-us/capabilities/geoai/overview',
-  'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/analysis',
-  'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/content',
-  'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/management',
-  'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/mapping',
-  'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/visualization-interpretation',
-  'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/overview',
-  'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/resources',
-  'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-data-collection',
-  'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-mapping-wayfinding',
-  'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-positioning-navigation',
-  'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/space-planning-reservation',
-  'https://www.esri.com/en-us/capabilities/indoor-gis/overview',
-  'https://www.esri.com/en-us/capabilities/mapping/overview',
-  'https://www.esri.com/en-us/capabilities/mapping/smart-mapping',
-  'https://www.esri.com/en-us/capabilities/real-time/features/actuation',
-  'https://www.esri.com/en-us/capabilities/real-time/features/analysis',
-  'https://www.esri.com/en-us/capabilities/real-time/features/dissemination',
-  'https://www.esri.com/en-us/capabilities/real-time/features/ingestion',
-  'https://www.esri.com/en-us/capabilities/real-time/features/storage',
-  'https://www.esri.com/en-us/capabilities/real-time/features/visualization',
-  'https://www.esri.com/en-us/capabilities/real-time/get-started',
-  'https://www.esri.com/en-us/capabilities/real-time/overview',
-  'https://www.esri.com/en-us/capabilities/real-time/partners/cloud-iot-platforms',
-  'https://www.esri.com/en-us/capabilities/real-time/partners/data',
-  'https://www.esri.com/en-us/capabilities/real-time/partners/service',
-  'https://www.esri.com/en-us/capabilities/spatial-analytics-data-science/analytics',
-  'https://www.esri.com/en-us/capabilities/spatial-analytics-data-science/overview',
-  'https://www.esri.com/en-us/artificial-intelligence/overview',
-  'https://www.esri.com/en-us/digital-transformation/overview',
-  'https://www.esri.com/en-us/digital-twin/overview',
-  'https://www.esri.com/en-us/location-intelligence/overview',
+  {
+    url: 'https://www.esri.com/en-us/about/about-esri/americas',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/about/about-esri/asia-pacific',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/about/about-esri/company',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/about/about-esri/europe/overview',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/about/about-esri/locations',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/about/about-esri/mea-central-asia',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/about/about-esri/overview',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/about/about-esri/technology',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/3d-gis/overview',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/3d-gis/resources',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/data-management',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/field-operations/get-started',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/field-operations/overview',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/geoai/overview',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/analysis',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/content',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/management',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/mapping',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/visualization-interpretation',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/overview',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/resources',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-data-collection',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-mapping-wayfinding',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-positioning-navigation',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/space-planning-reservation',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/indoor-gis/overview',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/mapping/overview',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/mapping/smart-mapping',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/features/actuation',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/features/analysis',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/features/dissemination',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/features/ingestion',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/features/storage',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/features/visualization',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/get-started',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/overview',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/partners/cloud-iot-platforms',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/partners/data',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/real-time/partners/service',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/spatial-analytics-data-science/analytics',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/capabilities/spatial-analytics-data-science/overview',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/artificial-intelligence/overview',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/digital-transformation/overview',
+    calcite_mode: 'light',
+  },
+  {
+    url: 'https://www.esri.com/en-us/digital-twin/overview',
+    calcite_mode: 'dark',
+  },
+  {
+    url: 'https://www.esri.com/en-us/location-intelligence/overview',
+    calcite_mode: 'dark',
+  },
 ];
