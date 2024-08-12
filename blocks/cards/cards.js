a7d6dc311e442e564cbf9211e806e15dbe6ef138
anu13297 <amallempati@esri.com>
2024-08-12 09:41:21 -0400

    #64-Cards simple variation (#77)
    * #64-Cards simple variation
    
    * updated to logical properties
    
    * updated card width
    
    * updates based on review comments
    
    * fix for lint error
    
    * updated JS to add a helper function
    
    * cards simple - fix for build errors
    
    * fix for trailing spaces
    
    * optimizing js for simple cards
    
    * removed unused style vars
    
    * updated css - cards simple

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,7 +1,26 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import { domEl } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   block.classList.add('calcite-mode-dark');
+
+  const processSimpleCard = function (div) {
+    if (!block.classList.contains('simple')) {
+      return;
+    }
+    const anchorEl = div.querySelector('a');
+    const cardBodyContent = domEl('div', { class: 'card-body-content' });
+    if (anchorEl) {
+      anchorEl.replaceChildren(cardBodyContent);
+      div.append(anchorEl);
+      div.querySelector('.button-container').remove();
+    } else {
+      div.append(cardBodyContent);
+    }
+    [...div.querySelectorAll('.cards-card-body > :not(.card-body-content, a)')].forEach((el) => {
+      cardBodyContent.append(el);
+    });
+  };
 
   /* change to ul, li */
   const ul = document.createElement('ul');
@@ -10,7 +29,8 @@ export default function decorate(block) {
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      else { div.className = 'cards-card-body'; }
+      processSimpleCard(div);
     });
     ul.append(li);
   });
