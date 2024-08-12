b48818921492493f5afe7d2fb282ce7f4b6a46df
Alex Carol <acarolcampre@adobe.com>
2024-08-12 18:34:08 +0200

    Update urls

---
+++ tools/importer/en_urls.js
@@ -0,0 +1,198 @@
+export default [
+  {
+    URL: 'https://www.esri.com/en-us/about/about-esri/americas',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/about/about-esri/asia-pacific',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/about/about-esri/company',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/about/about-esri/europe/overview',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/about/about-esri/locations',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/about/about-esri/mea-central-asia',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/about/about-esri/overview',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/about/about-esri/technology',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/3d-gis/overview',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/3d-gis/features/3d-data-management',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/3d-gis/features/3d-analysis',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/3d-gis/features/3d-visualization',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/3d-gis/features/3d-immersive-experiences',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/3d-gis/resources',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/data-management',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/field-operations/get-started',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/field-operations/overview',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/geoai/overview',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/analysis',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/content',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/management',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/mapping',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/visualization-interpretation',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/overview',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/imagery-remote-sensing/resources',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-data-collection',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-mapping-wayfinding',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-positioning-navigation',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/space-planning-reservation',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/indoor-gis/overview',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/mapping/overview',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/mapping/smart-mapping',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/features/actuation',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/features/analysis',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/features/dissemination',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/features/ingestion',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/features/storage',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/features/visualization',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/get-started',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/overview',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/partners/cloud-iot-platforms',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/partners/data',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/real-time/partners/service',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/spatial-analytics-data-science/analytics',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/capabilities/spatial-analytics-data-science/overview',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/artificial-intelligence/overview',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/digital-transformation/overview',
+    Theme: 'light',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/digital-twin/overview',
+    Theme: 'dark',
+  },
+  {
+    URL: 'https://www.esri.com/en-us/location-intelligence/overview',
+    Theme: 'dark',
+  },
+];
