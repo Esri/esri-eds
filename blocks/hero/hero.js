8a5b7e4537a7e8a643cc3af0b18270a28d24347b
Joe <jsavella@esri.com>
2024-08-20 07:33:52 -0700

    Hero - Foreground Image Markup Position is not static (#109)
    * add playhead functionality
    
    * lint error trailing spaces, missing ;
    
    * unexpected tab character, expected indentation of 2 spaces but found 1 tab
    
    * need more spacing
    
    * removing trailing spaces
    
    * opening curly brace does not appear on the same line
    
    * no space before curly brace
    
    * expected indentation of 4 spaces but found 2 tabs
    
    * more tabs
    
    * css fixes
    
    * combine overflow properties
    
    * combine overflow properties
    
    * re-order selectors
    
    * tbt issue
    
    * create function setVideoTag
    
    * unexpected tab character
    
    * async
    
    * async await
    
    * determine relative position of foreground image
    
    * remove extra spacing
    
    ---------
    
    Co-authored-by: Joe-Savella <josephsavella@yahoo.com>

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,16 +1,23 @@
 export default function decorate(block) {
-  block.querySelectorAll('picture > img').forEach((img) => {
+  block.querySelectorAll('picture > img').forEach((img, i) => {
     img.setAttribute('loading', 'eager');
+    if (i === 0) {
+      img.closest('p').classList.add('foreground-img');
+    }
   });
-
   const videoElement = document.createElement('video');
   const videoSrc = document.createElement('source');
   const videoAssets = block.querySelectorAll('a');
-
   videoElement.toggleAttribute('loop', true);
   videoElement.toggleAttribute('playsinline', true);
   videoElement.toggleAttribute('autoplay', true);
-  videoSrc.setAttribute('src', videoAssets[1].getAttribute('title'));
+  if (videoAssets.length === 2) {
+    videoSrc.setAttribute('src', videoAssets[1].getAttribute('title'));
+    videoAssets[1].classList.add('video-src');
+  } else {
+    videoSrc.setAttribute('src', videoAssets[0].getAttribute('title'));
+    videoAssets[0].classList.add('video-src');
+  }
   videoSrc.setAttribute('type', 'video/mp4');
 
   if (videoElement) videoElement.append(videoSrc);
--- blocks/storyteller/storyteller.js
+++ blocks/storyteller/storyteller.js
@@ -1,45 +1,124 @@
-export default function decorate(block) {
-  const pTags = block.querySelectorAll('p');
-  const pictureTagLeft = pTags[0].querySelector('picture');
-  const vidUrls = block.querySelectorAll('a');
-  const foregroundContentContainer = document.createElement('div');
+/**
+ * Determine if mp4 resource is available. Add selector 'foreground-container' to p tag.
+ * @param {string} vidUrls array with href property
+ * @returns {string} true/false
+ */
+function isMP4(vidUrls) {
+  const mp4Regex = /\.mp4$/;
+  let mp4Video = false;
+
+  vidUrls.forEach((url) => {
+    if (mp4Regex.test(url)) {
+      url.parentNode.classList.add('foreground-container');
+      mp4Video = true;
+    }
+  });
+
+  return mp4Video;
+}
+
+/**
+ * Toggle playhead to play or pause mp4 video.
+ * @param {element} videoBtn The playhead control element for mp4 video
+ */
+function toggleVideo(videoBtn) {
+  const foregroundWrapper = videoBtn.closest('.foreground-container');
+  const videoWrapper = foregroundWrapper.querySelector('.foreground-content');
+  const vidSrc = videoWrapper.querySelector('video');
+  if (vidSrc.paused === true) {
+    vidSrc.play();
+  } else {
+    vidSrc.pause();
+  }
+}
+
+/**
+ * Produce a calcite play button icon with appropriate attributes.
+ * @returns {element} play pause button
+ */
+async function getVideoBtn() {
+  const videoButton = document.createElement('button');
+  videoButton.classList.add('video-playbutton');
+  videoButton.setAttribute('aria-label', 'play or pause video');
+
+  const calciteIconPlayBtn = document.createElement('calcite-icon');
+  calciteIconPlayBtn.setAttribute('scale', 's');
+  calciteIconPlayBtn.setAttribute('appearance', 'solid');
+  calciteIconPlayBtn.setAttribute('icon', 'play-f');
+  calciteIconPlayBtn.setAttribute('aria-hidden', 'true');
+  videoButton.appendChild(calciteIconPlayBtn);
+
+  return videoButton;
+}
+
+async function setVideoTag(foregroundSrc) {
   const videoTag = document.createElement('video');
-  const foregroundPicture = block.querySelectorAll('picture')[1];
-  const foregroundSrc = foregroundPicture.querySelector('img').src;
-  const foregroundContent = document.createElement('div');
-  const source = document.createElement('source');
-  foregroundContent.classList.add('content-wrapper');
+
   videoTag.muted = true;
   videoTag.toggleAttribute('autoplay', 'true');
   videoTag.toggleAttribute('loop', true);
   videoTag.toggleAttribute('playsinline', true);
   videoTag.setAttribute('type', 'video/mp4');
   videoTag.setAttribute('poster', foregroundSrc);
+  return videoTag;
+}
 
-  if ((pictureTagLeft !== null) && (vidUrls.length >= 2)) {
+export default async function decorate(block) {
+  const pTags = block.querySelectorAll('p');
+  const pictureTagLeft = pTags[0].querySelector('picture');
+  const vidUrls = block.querySelectorAll('a');
+  const foregroundContentContainer = document.createElement('div');
+  const foregroundPicture = block.querySelectorAll('picture')[1];
+  const foregroundSrc = foregroundPicture.querySelector('img').src;
+  const foregroundContent = document.createElement('div');
+  const source = document.createElement('source');
+  const videoBtn = await getVideoBtn();
+  const videoTag = await setVideoTag(foregroundSrc);
+
+  foregroundContent.classList.add('content-wrapper');
+
+  if (isMP4(vidUrls) === true) {
+    foregroundPicture.classList.add('hide-poster');
+  }
+
+  if ((pictureTagLeft !== null) && (vidUrls.length >= 1)) {
+    const foregroundWrapper = block.querySelector('.foreground-container');
     const h2Tag = block.querySelector('h2');
+
     source.setAttribute('src', vidUrls[0].href);
     videoTag.appendChild(source);
-    block.classList.add('content-left');
-    pTags[1].classList.add('foreground-container');
+    block.classList.add('content-right');
     foregroundContentContainer.classList.add('foreground-content');
     foregroundContentContainer.appendChild(videoTag);
     foregroundContent.appendChild(h2Tag);
     foregroundContent.appendChild(pTags[2]);
     foregroundContentContainer.appendChild(foregroundContent);
-    pTags[1].appendChild(foregroundContentContainer);
+    foregroundWrapper.appendChild(foregroundContentContainer);
+    foregroundWrapper.appendChild(videoBtn);
   }
 
-  if ((pictureTagLeft === null) && (vidUrls.length >= 2)) {
+  if ((pictureTagLeft === null) && (vidUrls.length >= 1)) {
+    const foregroundWrapper = block.querySelector('.foreground-container');
     const h2Tags = block.querySelectorAll('h2');
+
     source.setAttribute('src', vidUrls[1].href);
     videoTag.appendChild(source);
-    pTags[3].classList.add('foreground-container');
     foregroundContentContainer.classList.add('foreground-content');
     foregroundContentContainer.appendChild(videoTag);
     foregroundContent.appendChild(h2Tags[1]);
-    foregroundContent.appendChild(pTags[4]);
+
+    if (pTags.length > 5) {
+      foregroundContent.appendChild(pTags[pTags.length - 1]);
+    } else {
+      foregroundContent.appendChild(pTags[4]);
+    }
+
     foregroundContentContainer.appendChild(foregroundContent);
-    pTags[3].appendChild(foregroundContentContainer);
+    foregroundWrapper.appendChild(foregroundContentContainer);
+    foregroundWrapper.appendChild(videoBtn);
   }
+
+  videoBtn.addEventListener('click', () => {
+    toggleVideo(videoBtn);
+  });
 }
