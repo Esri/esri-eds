3176adab461cccec81030aae445ffb13b752ca0f
hanem100k <abelsimon48@gmail.com>
2024-08-20 09:03:58 +0200

    load map  (#105)
    * feat: add map block WIP
    
    * chore: add missing map files
    
    * chore: switch to fetch from axios
    
    * fix: map overflow
    
    * chore: style inner container
    
    * chore: display points on map
    
    * feat: style popups wip
    
    * add fonts for icons
    
    * chore: further map styling
    
    * chore: load css on demand
    
    * chore: add fullscreen button
    
    * chore: parameterize map block
    
    * chore: white button on hover
    
    * chore: cleanup css
    
    * chore: extract heading from default content container
    
    * chore: add responsiveness to default content wrapper
    
    * feat: responsive map and content
    
    * chore: fix map responsiveness
    
    * chore: deselect global selectors
    
    * chore: lint fix
    
    * chore: observer load map
    
    * chore: embedd iframe container
    
    * chore: explicit async embedMapFrame
    
    * chore: explicit async embedMapFrame
    
    * chore: unused styling cleanup
    
    * chore: remove intersect observer. Restructure containers
    
    * chore: additional map restructure
    
    * chore: lint fix
    
    * chore: remove log
    
    * chore: address width responsiveness comments
    
    * chore: fix default content responsiveness
    
    * chore: fix paragraph styling
    
    * chore: temp remove fullscreen button
    
    * chore: button js and css cleanup
    
    * feat: add placeloader for iframe load
    
    * chore: overwrite h2 styling
    
    * feat: itnersection observer for map iframe
    
    * chore: lint
    
    * chore: function expression instead of declaration
    
    * chore: lint fi
    
    * chore: different threshold for the observer
    
    * chore: change observer threshold to .75 for mobile loading
    
    * chore: use arrow function instead of expression
    
    ---------
    
    Co-authored-by: Abel Simon <asimon@adobe.com>

--- blocks/map/map.js
+++ blocks/map/map.js
@@ -1,8 +1,22 @@
 import {
-  div, iframe, p, horizontalRule,
+  div, iframe, p, horizontalRule, domEl,
 } from '../../scripts/dom-helpers.js';
 
-function getMapFrame(url) {
+const getMapPlaceholder = () => {
+  const mapPlaceholder = domEl('calcite-loader', {
+    label: 'Loading Map',
+    class: 'iframe-map-placeholder',
+  });
+
+  return mapPlaceholder;
+};
+
+const handleLoadFrame = () => {
+  const mapPlaceholder = document.querySelector('.iframe-map-placeholder');
+  mapPlaceholder.style.display = 'none';
+};
+
+const getMapFrame = (url) => {
   const mapFrame = iframe({
     id: 'map-frame',
     class: 'map-frame-aspect-ratio',
@@ -13,10 +27,11 @@ function getMapFrame(url) {
     tabindex: '0',
     loading: 'lazy',
     title: 'Esri locations map',
+    onload: handleLoadFrame,
   });
 
   return mapFrame;
-}
+};
 
 export default async function decorate(block) {
   const blockParams = block.querySelectorAll('p');
@@ -27,7 +42,6 @@ export default async function decorate(block) {
 
   const gridContainer = div({ class: 'grid-container' });
   const frameWrapper = div({ id: 'frame-wrapper' });
-  frameWrapper.appendChild(getMapFrame(mapLink));
 
   const defaultContentContainer = document.querySelector('.map-container > .default-content-wrapper');
   const nodeTextParam = p({ id: 'map-text-content' });
@@ -46,6 +60,23 @@ export default async function decorate(block) {
   gridContainerChildren.forEach((child) => {
     gridContainer.appendChild(child);
   });
+
+  const observer = new IntersectionObserver((entries) => {
+    const frameContainer = entries[0];
+
+    if (!frameContainer.isIntersecting) return;
+
+    entries.forEach((e) => {
+      if (e.isIntersecting) {
+        frameWrapper.appendChild(getMapPlaceholder());
+        frameWrapper.appendChild(getMapFrame(mapLink));
+        observer.unobserve(e.target);
+      }
+    });
+    observer.disconnect();
+  }, { threshold: 0.75 });
+
+  observer.observe(frameWrapper);
 
   block.append(gridContainer);
 }
