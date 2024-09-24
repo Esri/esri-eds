1a6569694ad30da3b1d713f32196c634d645dc9b
anu13297 <amallempati@esri.com>
2024-09-24 08:33:03 -0700

    Cards Standard Variation (#129)
    * Cards Standard
    
    * update to cards per row
    
    * updated the condition for calling simple and standard

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,10 +3,6 @@ import { domEl } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   const processSimpleCard = (div) => {
-    if (!block.classList.contains('simple')) {
-      return;
-    }
-    block.classList.add('cardsperrow');
     const anchorEl = div.querySelector('a');
     const cardBodyContent = domEl('div', { class: 'card-body-content' });
     if (anchorEl) {
@@ -21,6 +17,35 @@ export default function decorate(block) {
     });
   };
 
+  const processStandardCard = (div) => {
+    const anchorEl = div.querySelector('a');
+    const cardBodyContent = domEl('div', { class: 'card-body-content' });
+    if (anchorEl) {
+      anchorEl.replaceChildren(cardBodyContent);
+      div.append(anchorEl);
+    } else {
+      div.append(cardBodyContent);
+    }
+    div.querySelectorAll('p').forEach((p) => { if (p.textContent === '') p.remove(); });
+    [...div.querySelectorAll('.cards-card-body > :not(.card-body-content, a)')].forEach((el) => {
+      cardBodyContent.append(el);
+      if (el.tagName === 'P' && el.children.length === 0 && el.parentNode.firstElementChild === el) {
+        el.classList.add('overlay-text');
+      }
+    });
+    const pictureEl = div.querySelector('picture').closest('p');
+    const overlayTextEl = div.querySelector('.overlay-text');
+    if (overlayTextEl) pictureEl.append(overlayTextEl);
+    pictureEl.nextElementSibling.classList.add('card-body-title');
+    const cardBodyTitle = div.querySelector('.card-body-title');
+
+    if (cardBodyContent.lastChild.classList === '') cardBodyContent.lastChild.classList.add('card-body-description');
+
+    if (cardBodyTitle.nextElementSibling && !cardBodyTitle.nextElementSibling.classList.contains('card-body-description')) {
+      cardBodyTitle.nextElementSibling.classList.add('card-body-subtitle');
+    }
+  };
+
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
@@ -29,7 +54,8 @@ export default function decorate(block) {
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else { div.className = 'cards-card-body'; }
-      processSimpleCard(div);
+      if (block.classList.contains('simple')) processSimpleCard(div);
+      if (block.classList.contains('standard')) processStandardCard(div);
     });
     ul.append(li);
   });
