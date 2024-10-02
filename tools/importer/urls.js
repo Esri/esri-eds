b5184c14d9c40e599263d791c4370c911ce9cfc9
Alex Carol <acarolcampre@adobe.com>
2024-10-02 11:09:46 +0200

    Use french urls

--- tools/importer/urls.js
+++ tools/importer/urls.js
@@ -1,394 +1,198 @@
 export default [
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/about/about-esri/americas"
+    "URL": "https://www.esri.com/fr-fr/about/about-esri/americas"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/about/about-esri/asia-pacific"
+    "URL": "https://www.esri.com/fr-fr/about/about-esri/asia-pacific"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/about/about-esri/company"
+    "URL": "https://www.esri.com/fr-fr/about/about-esri/company"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/about/about-esri/europe/overview"
+    "URL": "https://www.esri.com/fr-fr/about/about-esri/europe/overview"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/about/about-esri/locations"
+    "URL": "https://www.esri.com/fr-fr/about/about-esri/locations"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/about/about-esri/mea-central-asia"
+    "URL": "https://www.esri.com/fr-fr/about/about-esri/mea-central-asia"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/about/about-esri/overview"
+    "URL": "https://www.esri.com/fr-fr/about/about-esri/overview"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/about/about-esri/technology"
+    "URL": "https://www.esri.com/fr-fr/about/about-esri/technology"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/3d-gis/overview"
+    "URL": "https://www.esri.com/fr-fr/capabilities/3d-gis/overview"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/3d-gis/features/3d-data-management"
+    "URL": "https://www.esri.com/fr-fr/capabilities/3d-gis/features/3d-data-management"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/3d-gis/features/3d-analysis"
+    "URL": "https://www.esri.com/fr-fr/capabilities/3d-gis/features/3d-analysis"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/3d-gis/features/3d-visualization"
+    "URL": "https://www.esri.com/fr-fr/capabilities/3d-gis/features/3d-visualization"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/3d-gis/features/3d-immersive-experiences"
+    "URL": "https://www.esri.com/fr-fr/capabilities/3d-gis/features/3d-immersive-experiences"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/3d-gis/resources"
+    "URL": "https://www.esri.com/fr-fr/capabilities/3d-gis/resources"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/data-management"
+    "URL": "https://www.esri.com/fr-fr/capabilities/data-management"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/field-operations/get-started"
+    "URL": "https://www.esri.com/fr-fr/capabilities/field-operations/get-started"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/field-operations/overview"
+    "URL": "https://www.esri.com/fr-fr/capabilities/field-operations/overview"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/geoai/overview"
+    "URL": "https://www.esri.com/fr-fr/capabilities/geoai/overview"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/analysis"
+    "URL": "https://www.esri.com/fr-fr/capabilities/imagery-remote-sensing/capabilities/analysis"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/content"
+    "URL": "https://www.esri.com/fr-fr/capabilities/imagery-remote-sensing/capabilities/content"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/management"
+    "URL": "https://www.esri.com/fr-fr/capabilities/imagery-remote-sensing/capabilities/management"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/mapping"
+    "URL": "https://www.esri.com/fr-fr/capabilities/imagery-remote-sensing/capabilities/mapping"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/imagery-remote-sensing/capabilities/visualization-interpretation"
+    "URL": "https://www.esri.com/fr-fr/capabilities/imagery-remote-sensing/capabilities/visualization-interpretation"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/imagery-remote-sensing/overview"
+    "URL": "https://www.esri.com/fr-fr/capabilities/imagery-remote-sensing/overview"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/imagery-remote-sensing/resources"
+    "URL": "https://www.esri.com/fr-fr/capabilities/imagery-remote-sensing/resources"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-data-collection"
+    "URL": "https://www.esri.com/fr-fr/capabilities/indoor-gis/capabilities/indoor-data-collection"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-mapping-wayfinding"
+    "URL": "https://www.esri.com/fr-fr/capabilities/indoor-gis/capabilities/indoor-mapping-wayfinding"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/indoor-positioning-navigation"
+    "URL": "https://www.esri.com/fr-fr/capabilities/indoor-gis/capabilities/indoor-positioning-navigation"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/indoor-gis/capabilities/space-planning-reservation"
+    "URL": "https://www.esri.com/fr-fr/capabilities/indoor-gis/capabilities/space-planning-reservation"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/indoor-gis/overview"
+    "URL": "https://www.esri.com/fr-fr/capabilities/indoor-gis/overview"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/mapping/overview"
+    "URL": "https://www.esri.com/fr-fr/capabilities/mapping/overview"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/mapping/smart-mapping"
+    "URL": "https://www.esri.com/fr-fr/capabilities/mapping/smart-mapping"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/features/actuation"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/features/actuation"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/features/analysis"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/features/analysis"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/features/dissemination"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/features/dissemination"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/features/ingestion"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/features/ingestion"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/features/storage"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/features/storage"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/features/visualization"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/features/visualization"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/get-started"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/get-started"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/overview"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/overview"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/partners/cloud-iot-platforms"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/partners/cloud-iot-platforms"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/partners/data"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/partners/data"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/capabilities/real-time/partners/service"
+    "URL": "https://www.esri.com/fr-fr/capabilities/real-time/partners/service"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/spatial-analytics-data-science/analytics"
+    "URL": "https://www.esri.com/fr-fr/capabilities/spatial-analytics-data-science/analytics"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/capabilities/spatial-analytics-data-science/overview"
+    "URL": "https://www.esri.com/fr-fr/capabilities/spatial-analytics-data-science/overview"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/artificial-intelligence/overview"
+    "URL": "https://www.esri.com/fr-fr/artificial-intelligence/overview"
   },
   {
     "Theme": "light",
-    "URL": "https://www.esri.com/en-us/digital-transformation/overview"
+    "URL": "https://www.esri.com/fr-fr/digital-transformation/overview"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/digital-twin/overview"
+    "URL": "https://www.esri.com/fr-fr/digital-twin/overview"
   },
   {
     "Theme": "dark",
-    "URL": "https://www.esri.com/en-us/location-intelligence/overview"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/about/about-esri/americas"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/about/about-esri/asia-pacific"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/about/about-esri/company"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/about/about-esri/europe/overview"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/about/about-esri/locations"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/about/about-esri/mea-central-asia"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/about/about-esri/overview"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/about/about-esri/technology"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/3d-gis/overview"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/3d-gis/features/3d-data-management"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/3d-gis/features/3d-analysis"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/3d-gis/features/3d-visualization"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/3d-gis/features/3d-immersive-experiences"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/3d-gis/resources"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/data-management"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/field-operations/get-started"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/field-operations/overview"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/geoai/overview"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/imagery-remote-sensing/capabilities/analysis"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/imagery-remote-sensing/capabilities/content"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/imagery-remote-sensing/capabilities/management"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/imagery-remote-sensing/capabilities/mapping"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/imagery-remote-sensing/capabilities/visualization-interpretation"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/imagery-remote-sensing/overview"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/imagery-remote-sensing/resources"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/indoor-gis/capabilities/indoor-data-collection"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/indoor-gis/capabilities/indoor-mapping-wayfinding"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/indoor-gis/capabilities/indoor-positioning-navigation"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/indoor-gis/capabilities/space-planning-reservation"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/indoor-gis/overview"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/mapping/overview"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/mapping/smart-mapping"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/features/actuation"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/features/analysis"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/features/dissemination"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/features/ingestion"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/features/storage"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/features/visualization"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/get-started"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/overview"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/partners/cloud-iot-platforms"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/partners/data"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/capabilities/real-time/partners/service"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/spatial-analytics-data-science/analytics"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/capabilities/spatial-analytics-data-science/overview"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/artificial-intelligence/overview"
-  },
-  {
-    "Theme": "light",
-    "URL": "https://www.esri.com/ar-sa/digital-transformation/overview"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/digital-twin/overview"
-  },
-  {
-    "Theme": "dark",
-    "URL": "https://www.esri.com/ar-sa/location-intelligence/overview"
+    "URL": "https://www.esri.com/fr-fr/location-intelligence/overview"
   }
 ]
