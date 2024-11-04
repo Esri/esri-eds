b34bad96ab7eead9fb25477ed50f4c3352259da9
Tim White <tim_white@esri.com>
2024-11-04 11:06:40 -0800

    fix: spacing

--- eds/blocks/call-to-action/call-to-action.js
+++ eds/blocks/call-to-action/call-to-action.js
@@ -7,35 +7,34 @@ export default async function decorate(block) {
   block.querySelectorAll('p.button-container').forEach((buttonContainer) => {
     // create a new div to replace the buttonContainer
     const newDiv = div({ class: 'button-container' });
-      while (buttonContainer.firstChild) {
-      // if firstchild is <a> then construct calciteButton
-        if (buttonContainer.firstChild.tagName === 'A') {
-          newDiv.appendChild(calciteButton({
-            appearance: 'solid',
-            color: 'blue',
-            kind: 'brand',
-            scale: 'm',
-            href: buttonContainer.firstChild.href,
-            alignment: 'center',
-            type: 'button',
-            width: 'auto',
-          }, buttonContainer.firstChild.textContent));
-        } else if (buttonContainer.firstChild.tagName === 'EM') {
-          newDiv.appendChild(calciteButton({
-            appearance: 'outline',
-            color: 'blue',
-            kind: 'brand',
-            scale: 'm',
-            href: buttonContainer.firstChild.href,
-            alignment: 'center',
-            type: 'button',
-            width: 'auto',
-          }, buttonContainer.firstChild.textContent));
-        }
-
-        buttonContainer.removeChild(buttonContainer.firstChild);
+    while (buttonContainer.firstChild) {
+    // if firstchild is <a> then construct calciteButton
+      if (buttonContainer.firstChild.tagName === 'A') {
+        newDiv.appendChild(calciteButton({
+          appearance: 'solid',
+          color: 'blue',
+          kind: 'brand',
+          scale: 'm',
+          href: buttonContainer.firstChild.href,
+          alignment: 'center',
+          type: 'button',
+          width: 'auto',
+        }, buttonContainer.firstChild.textContent));
+      } else if (buttonContainer.firstChild.tagName === 'EM') {
+        newDiv.appendChild(calciteButton({
+          appearance: 'outline',
+          color: 'blue',
+          kind: 'brand',
+          scale: 'm',
+          href: buttonContainer.firstChild.href,
+          alignment: 'center',
+          type: 'button',
+          width: 'auto',
+        }, buttonContainer.firstChild.textContent));
       }
-      buttonContainer.replaceWith(newDiv);
-    });
+      buttonContainer.removeChild(buttonContainer.firstChild);
+    }
+    buttonContainer.replaceWith(newDiv);
+  });
   return block;
 }
