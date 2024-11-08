d50e70eea94471a9b9b87a411fe2248515369c2b
Tim White <tim_white@esri.com>
2024-11-08 13:33:52 -0800

    feat: update to allow two images

--- eds/blocks/large-content-stack/large-content-stack.js
+++ eds/blocks/large-content-stack/large-content-stack.js
@@ -55,26 +55,50 @@ export default function decorate(block) {
   convertToCalciteButton(button);
 
   // TODO background picture quality is low, fix it
-  const backgroundPicture = mainCell.querySelector(':scope > p:last-child:has(+ p > picture) > picture');
-  if (backgroundPicture) {
-    const backgroundPictureSrc = backgroundPicture
-      .querySelector('source')
-      .srcset
-      .replace('optimize=medium', 'optimize=false');
+  // Select all p:last-child > picture elements
+  const pictures = document.querySelectorAll('p:last-child > picture');
 
-    block.style.backgroundImage = `url(${backgroundPictureSrc})`;
-    backgroundPicture.parentElement.remove();
-  }
+  pictures.forEach((picture) => {
+    const parentP = picture.parentElement;
 
-  const picture = block.querySelector('p > picture');
-  console.log('Picture: ' + picture);
-  const pictureWrapper = picture.parentElement;
-  const mediaWrapper = div(
-    { class: 'media-wrapper' },
-    picture,
-  );
+    // Check if the parent p element has a preceding sibling that contains a picture element
+    let hasPrecedingSiblingWithPicture = false;
+    let sibling = parentP.previousElementSibling;
+    while (sibling) {
+      if (sibling.querySelector('picture')) {
+        hasPrecedingSiblingWithPicture = true;
+        break;
+      }
+      sibling = sibling.previousElementSibling;
+    }
+
+    // If the condition is met, do something with the picture element
+    if (hasPrecedingSiblingWithPicture) {
+      console.log('Found a matching picture element:', picture);
+      const backgroundPicture = mainCell.querySelector(':scope > p:last-child:has(+ p > picture) > picture');
+      if (backgroundPicture) {
+        const backgroundPictureSrc = backgroundPicture
+          .querySelector('source')
+          .srcset
+          .replace('optimize=medium', 'optimize=false');
+    
+        block.style.backgroundImage = `url(${backgroundPictureSrc})`;
+        backgroundPicture.parentElement.remove();
+      }
+    } else {
+      console.log('No matching picture element found:', picture);
+      const pictureWrapper = picture.parentElement;
+      const mediaWrapper = div(
+        { class: 'media-wrapper' },
+        picture,
+      );
+      pictureWrapper.replaceWith(mediaWrapper);
+    }
+  });
+
+  // const picture = block.querySelector('p > picture');
+  
   if (videoElement) {
     mediaWrapper.appendChild(videoElement);
   }
-  pictureWrapper.replaceWith(mediaWrapper);
 }
