0abd5f63d07621390f3ac33b45d8dd0a92d17b6c
Tim White <tim_white@esri.com>
2024-11-08 14:22:26 -0800

    feat: check for two images, set 2nd to background if present

--- eds/blocks/large-content-stack/large-content-stack.js
+++ eds/blocks/large-content-stack/large-content-stack.js
@@ -55,14 +55,18 @@ export default function decorate(block) {
   convertToCalciteButton(button);
 
   // TODO background picture quality is low, fix it
-  // Select all p:last-child > picture elements
-  const pictures = document.querySelectorAll('p:last-child > picture');
-
+  // If one image, use as foreground image
+  // If two images, second image is background image
+  const pictures = document.querySelectorAll('p > picture');
   pictures.forEach((picture) => {
     const parentP = picture.parentElement;
 
+    // Check if the parent p element is the last child
+    const isLastChild = parentP === parentP.parentElement.lastElementChild;
+
     // Check if the parent p element has a preceding sibling that contains a picture element
     let hasPrecedingSiblingWithPicture = false;
+
     let sibling = parentP.previousElementSibling;
     while (sibling) {
       if (sibling.querySelector('picture')) {
@@ -72,21 +76,15 @@ export default function decorate(block) {
       sibling = sibling.previousElementSibling;
     }
 
-    // If the condition is met, do something with the picture element
-    if (hasPrecedingSiblingWithPicture) {
-      console.log('Found a matching picture element:', picture);
-      const backgroundPicture = mainCell.querySelector(':scope > p:last-child:has(+ p > picture) > picture');
-      if (backgroundPicture) {
-        const backgroundPictureSrc = backgroundPicture
-          .querySelector('source')
-          .srcset
-          .replace('optimize=medium', 'optimize=false');
-    
-        block.style.backgroundImage = `url(${backgroundPictureSrc})`;
-        backgroundPicture.parentElement.remove();
-      }
+    if (isLastChild && hasPrecedingSiblingWithPicture) {
+      const backgroundPictureSrc = picture
+        .querySelector('source')
+        .srcset
+        .replace('optimize=medium', 'optimize=false');
+
+      block.style.backgroundImage = `url(${backgroundPictureSrc})`;
+      parentP.remove();
     } else {
-      console.log('No matching picture element found:', picture);
       const pictureWrapper = picture.parentElement;
       const mediaWrapper = div(
         { class: 'media-wrapper' },
@@ -96,8 +94,6 @@ export default function decorate(block) {
     }
   });
 
-  // const picture = block.querySelector('p > picture');
-  
   if (videoElement) {
     mediaWrapper.appendChild(videoElement);
   }
