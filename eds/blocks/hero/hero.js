ea8f782a7bbfea771f67f5a0a6c73ad7168e8c71
Tim White <tim_white@esri.com>
2024-11-15 13:48:28 -0800

    feat: initial

--- eds/blocks/hero/hero.js
+++ eds/blocks/hero/hero.js
@@ -1,12 +1,20 @@
 import { domEl } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
-  block.querySelectorAll('picture > img').forEach((img, i) => {
-    img.setAttribute('loading', 'eager');
-    if (i === 0) {
-      img.closest('p').classList.add('foreground-img');
-    }
-  });
+  // find all children of the block
+  // const blockChildren = [...block.children];
+  // in blockChildren find second child of each
+  // const children = blockChildren.map((child) => child.children[1]);
+
+
+  // const imgCollection = block.querySelectorAll('picture > img');
+  // imgCollection.forEach((img, i) => {
+  //   img.setAttribute('loading', 'eager');
+  //   if (i === 0) {
+  //     img.closest('p').classList.add('foreground-img');
+  //   }
+  // });
+  
   const videoElement = document.createElement('video');
   const videoSrc = document.createElement('source');
   const videoAssets = block.querySelectorAll('a');
@@ -25,20 +33,16 @@ export default function decorate(block) {
   videoSrc.setAttribute('type', 'video/mp4');
 
   if (videoElement) videoElement.append(videoSrc);
-  // if (videoAssets) block.prepend(videoElement);
+  if (videoAssets) block.prepend(videoElement);
 
   const contentDiv = block.querySelector('div:last-child');
+  console.log(contentDiv);
   const contentChildDiv = contentDiv.querySelector('div');
   const children = [...contentChildDiv.children];
 
   const heroContent = domEl('div', { class: 'hero-content' });
   const heroContentWrapper = domEl('div', { class: 'hero-content-wrapper' });
   const heroFgImage = domEl('div', { class: 'hero-fg-image' });
-
-  // If there is no image, assume video is the foreground image
-  if (heroFgImage.children.length === 0) {
-    heroFgImage.appendChild(videoElement);
-  }
 
   heroContent.prepend(heroContentWrapper);
   block.prepend(heroContent, heroFgImage);
@@ -50,5 +54,5 @@ export default function decorate(block) {
       heroFgImage.appendChild(child); // Move .foreground-img to right container
     }
   });
-  contentDiv.remove(); // Remove the empty div
+  //contentDiv.remove(); // Remove the empty div
 }
