4cb73097c423c9cefd208ea5833e2038c17c9eb7
Joe-Savella <josephsavella@yahoo.com>
2024-12-17 15:10:41 -0800

    find overview page for each folder

--- eds/blocks/local-navigation/local-navigation.js
+++ eds/blocks/local-navigation/local-navigation.js
@@ -51,13 +51,23 @@ function listenSubNav(subNavItems) {
       subNavItems.nextElementSibling.setAttribute('aria-hidden', 'true');
     }
   });
+
+  document.addEventListener('click', (e) => {
+    let isClickInside = subNavItems.contains(e.target);
+
+    if (!isClickInside) { 
+      subNavItems.setAttribute('aria-expanded', 'false');
+      subNavItems.nextElementSibling.setAttribute('aria-hidden', 'true');
+    }
+});
 }
 
 /**
  * Create a new list item for page title and append to the nav tag ul
+ * Create subnavItems if exist and append to the li
  * @param {Object, Element} pgObj The page title and page url.
  */
-function appendPageTitle(pgObj, block, i) {
+function appendPageTitle(pgObj, block, i, menuTitle) {
   const updatedUrl = updateURL(pgObj.pageLink);
   const navTagUL = block.querySelector('ul');
   const li = domEl('li', { class: 'page-title', id: pgObj.pageTitle });
@@ -67,14 +77,17 @@ function appendPageTitle(pgObj, block, i) {
   aHref.innerHTML = `${pgObj.pageTitle}`;
   aHref.setAttribute('aria-current', 'false');
 
-  if (currPageTitle === pgObj.pageTitle.toLowerCase()) {
+  if ((currPageTitle === pgObj.pageTitle.toLowerCase() || menuTitle === pgObj.pageTitle) && !pgObj.subnavItems) {
     aHref.setAttribute('aria-current', 'true');
   }
+
+  /* if subnavItems exist, create a button for the subnav and append to the li */
   if (pgObj.subnavItems) {
     const subNavItems = domEl('button', {
       class: 'subnav-btn',
       'aria-expanded': 'false',
       'aria-controls': 'subnav',
+      'aria-current': 'false',
       'attr-order': i,
     });
     const subNav = domEl('div', { class: 'subnav', id: 'subnav', 'aria-hidden': 'true' });
@@ -89,6 +102,9 @@ function appendPageTitle(pgObj, block, i) {
       subNavA.innerHTML = `${item.pageTitle}`;
       subNavLI.appendChild(subNavA);
       subNavUL.appendChild(subNavLI);
+      if (currPageTitle === item.pageTitle.toLowerCase() || menuTitle === item.pageTitle) {
+        subNavItems.setAttribute('aria-current', 'true');
+      }
     });
     subNav.appendChild(subNavUL);
     li.appendChild(subNav);
@@ -126,14 +142,14 @@ function decorateBlueButton(value, block) {
  * Normalize url path, replace origin if different current origin.
  * @param {JSON, Element} xmlData The api returned xmlData page folder json schema.
  */
-function parseXML(xmlData, block) {
+function parseXML(xmlData, block, menuTitle) {
   for (let i = 0; i < xmlData.length; i += 1) {
     Object.entries(xmlData[i]).forEach(([key, value]) => {
       if (key === 'main') {
         navigationTitle(value, block);
       }
       if (key === 'pageTitle') {
-        appendPageTitle(xmlData[i], block, i);
+        appendPageTitle(xmlData[i], block, i, menuTitle);
       }
       if (value.triallabel) {
         decorateBlueButton(value, block);
@@ -196,6 +212,7 @@ function initNavWrapper(block) {
   const trialBtn = btnWrapper.lastElementChild;
   mobileButton.setAttribute('aria-label', 'menu');
   htmlNavTag.setAttribute('aria-label', 'main');
+  htmlNavTag.setAttribute('class', 'calcite-mode-light');
   htmlNavTag.id = 'main';
   ul.classList.add('mobile-menu');
   ul.setAttribute('aria-labelledby', 'nav-title');
@@ -211,7 +228,7 @@ function initNavWrapper(block) {
  * toggle caret-up or caret-down mobile menu caret icon and aria-expanded attribute
  * @param {Element} block The header block element
  */
-function btnEventListener(block) {
+function ctaEventListener(block) {
   const mobileBtn = block.querySelector('calcite-icon.btn-mobile');
   const mobileMenu = block.querySelector('ul.mobile-menu');
 
@@ -220,12 +237,6 @@ function btnEventListener(block) {
       mobileBtn.setAttribute('icon', 'caret-up');
       mobileMenu.setAttribute('aria-expanded', 'true');
     } else {
-      mobileBtn.setAttribute('icon', 'caret-down');
-      mobileMenu.setAttribute('aria-expanded', 'false');
-    }
-  });
-  window.addEventListener('resize', () => {
-    if (window.innerWidth > 768) {
       mobileBtn.setAttribute('icon', 'caret-down');
       mobileMenu.setAttribute('aria-expanded', 'false');
     }
@@ -240,9 +251,44 @@ function resetDropdown(block) {
   const mobileBtn = block.querySelector('calcite-icon.btn-mobile');
   const mobileMenu = block.querySelector('ul.mobile-menu');
   window.addEventListener('resize', () => {
-    mobileBtn.setAttribute('icon', 'caret-down');
-    mobileMenu.setAttribute('aria-expanded', 'false');
-  }, 500);
+    if (window.innerWidth > 768) {
+      mobileBtn.setAttribute('icon', 'caret-down');
+      mobileMenu.setAttribute('aria-expanded', 'false');
+    }
+  });
+}
+
+/**
+ * Read the id='breadcrumbs' script tag and append to the header block
+ * @param {Element} block The header block element
+ */
+function getBreadcrumb() {
+  if (!document.querySelector('script#breadcrumbs')) return;
+  const scriptTag = document.querySelector('script#breadcrumbs');
+  const jsonData = JSON.parse(scriptTag.textContent || scriptTag.innerText);
+  return jsonData.itemListElement[jsonData.itemListElement.length-1]
+}
+
+function fetchNavData(block) {
+  const ISLOCAL = /localhost/gm;
+  const PROXY = ISLOCAL.test(window.location.href) ? 'https://cors-anywhere.herokuapp.com/' : '';
+  const NAVAPI = 'https://www.esri.com/bin/esri/localnavigation';
+  const pathURL = getBreadcrumb() || window.location.pathname;
+  const requestURL = `${PROXY}${NAVAPI}?path=/content/esri-sites${pathURL.item}`;
+  const breadcrumbs = getBreadcrumb();
+  const menuTitle = breadcrumbs.name;
+  fetch(requestURL)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      initNavWrapper(block);
+      parseXML(jsonData, block, menuTitle);
+      docAuthPageTitle(block);
+      ctaEventListener(block);
+      resetDropdown(block);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 }
 
 /**
@@ -250,18 +296,19 @@ function resetDropdown(block) {
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
-  const ISLOCAL = /localhost/gm;
-  const PROXY = ISLOCAL.test(window.location.href) ? 'https://cors-anywhere.herokuapp.com/' : '';
-  const NAVAPI = 'https://www.esri.com/bin/esri/localnavigation';
-  const requestURL = `${PROXY}${NAVAPI}?path=/content/esri-sites${window.location.pathname}`;
-  await fetch(requestURL)
-    .then((response) => response.json())
-    .then((data) => {
-      initNavWrapper(block);
-      parseXML(data, block);
-      docAuthPageTitle(block);
-      btnEventListener(block);
-      resetDropdown(block);
-    })
-    .catch((error) => error);
+  if (!getBreadcrumb()) {
+    let count = 0;
+    const interval = setInterval(() => {
+      if (getBreadcrumb()) {
+        clearInterval(interval);
+        fetchNavData(block);
+      }
+      count += 1;
+      if (count === 20) {
+        clearInterval(interval);
+      }
+    }, 500);
+  } else {
+    fetchNavData(block);
+  }
 }
