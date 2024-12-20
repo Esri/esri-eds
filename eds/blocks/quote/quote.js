50c1f3523e18b41b030320e747bc4508b9e3a754
Tim White <tim_white@esri.com>
2024-12-19 16:11:08 -0800

    feat: cleanup and simplify css after new import

--- eds/blocks/quote/quote.js
+++ eds/blocks/quote/quote.js
@@ -1,8 +1,9 @@
 import { decorateIcons } from '../../scripts/scripts.js';
-import { span, ul, li } from '../../scripts/dom-helpers.js';
+import { span, article, div } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
-  const list = ul(...[...block.children].map((row) => li(...[...row.children].map((divElement) => {
+  // eslint-disable-next-line max-len
+  const list = article(...[...block.children].map((row) => div(...[...row.children].map((divElement) => {
     if (
       divElement.children.length === 1
       && divElement.querySelector('picture')
