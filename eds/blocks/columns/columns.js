bede49cd9d622b34175c9642b568742e167e300f
Joe-Savella <josephsavella@yahoo.com>
2025-01-03 16:00:56 -0800

    align ctas in a row

--- eds/blocks/columns/columns.js
+++ eds/blocks/columns/columns.js
@@ -13,6 +13,17 @@ export default function decorate(block) {
           picWrapper.classList.add('columns-img-col');
         }
       }
+
+      const buttons = col.querySelectorAll('.button-container');
+      if (buttons.length > 0) {
+        const parent = buttons[0].parentElement;
+        const buttonWrapper = document.createElement('div');
+        buttonWrapper.classList.add('buttons-container');
+        buttons.forEach((btn) => {
+          buttonWrapper.appendChild(btn);
+        });
+        parent.appendChild(buttonWrapper);
+      }
     });
   });
 }
