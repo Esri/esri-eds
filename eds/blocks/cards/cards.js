2170ba3152817096eef71069413779fc163f3c09
anu13297 <amallempati@esri.com>
2025-02-24 14:27:10 -0500

    fix for lint errors

--- eds/blocks/cards/cards.js
+++ eds/blocks/cards/cards.js
@@ -1,6 +1,34 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-import { div, domEl, calciteLink, calciteButton } from '../../scripts/dom-helpers.js';
+import {
+  div,
+  domEl,
+  calciteLink,
+  calciteButton,
+} from '../../scripts/dom-helpers.js';
 import decorateModal from '../../scripts/delayed.js';
+
+function videoCards(anchorEl) {
+  const playButton = calciteButton({
+    'icon-start': 'play-f',
+    'aria-hidden': 'false',
+    'aria-label': 'play video',
+    class: 'play-button',
+    label: 'play video',
+    appearance: 'solid',
+    scale: 'l',
+    round: '',
+    color: 'light',
+    kind: '',
+  });
+
+  anchorEl.querySelector('picture').parentElement.append(playButton);
+
+  playButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    playButton.setAttribute('tabindex', '0');
+    decorateModal(anchorEl.href, playButton);
+  });
+}
 
 function processSimpleCard(listElem) {
   const cardBody = listElem.querySelector('.cards-card-body');
@@ -60,32 +88,9 @@ function processStandardCard(element) {
       cardBodyTitle.nextElementSibling.classList.add('card-body-subtitle');
     }
   }
-  if(anchorEl.classList.contains('video-link')) {
+  if (anchorEl.classList.contains('video-link')) {
     videoCards(anchorEl);
   }
-}
-
-function videoCards(anchorEl) {
-  const playButton = calciteButton({
-    'icon-start': 'play-f',
-    'aria-hidden': 'false',
-    'aria-label': 'play video',
-    class: 'play-button',
-    label: 'play video',
-    appearance: 'solid',
-    scale: 'l',
-    round: '',
-    color: 'light',
-    kind: '',
-  });
-
-  anchorEl.querySelector('picture').parentElement.append(playButton);
-
-  playButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    playButton.setAttribute('tabindex', '0');
-    decorateModal(anchorEl.href, playButton);
-  });
 }
 
 export default function decorate(block) {
