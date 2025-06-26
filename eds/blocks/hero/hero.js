6579e6f75ada2249b83dad73bae073b1950b1cf2
Anuhya Mallempati <amallempati@esri.com>
2025-06-26 15:45:11 -0400

    fix(ui): Added play pause controls to hero bg video[#717] (#741)
    * fix(ui): Added play pause controls to hero bg video
    
    * fix(ui): fixed the lint errors by removing empty lines

--- eds/blocks/hero/hero.js
+++ eds/blocks/hero/hero.js
@@ -1,6 +1,150 @@
 import { createAutoplayedVideo } from '../../scripts/scripts.js';
-import { calciteButton } from '../../scripts/dom-helpers.js';
+import { calciteButton, video, button } from '../../scripts/dom-helpers.js';
 import decorateModal from '../../scripts/delayed.js';
+
+/**
+ * Produce a calcite play button icon with appropriate attributes.
+ *
+ * @returns {HTMLButtonElement} A play/pause button
+ */
+function getVideoBtn() {
+  const videoButton = button({
+    class: 'video-play-button', 'aria-label': 'Play animation', tabindex: '0',
+  });
+
+  videoButton.innerHTML = `<svg class="play-progress-circle" viewBox="0 0 100 100">
+  <circle class="progress-background" r="45" cx="50" cy="50"></circle>
+  <circle class="progress-circle" r="41.25" cx="50" cy="50"></circle>
+</svg>`;
+  return videoButton;
+}
+
+/**
+ * Get all mp4 URLs from the block.
+ *
+ * @param {Element} block - The block element
+ *
+ * @returns {Array.<HTMLAnchorElement>} The array of mp4 urls
+ */
+function getVideoUrls(block) {
+  const aTags = block.querySelectorAll('a');
+  const mp4Urls = [];
+
+  aTags.forEach((aTag) => {
+    if (aTag.href.includes('.mp4')) {
+      mp4Urls.push(aTag);
+    }
+  });
+
+  return mp4Urls;
+}
+
+/**
+ * Play a video element with a promise, handling potential errors.
+ *
+ * @param {HTMLVideoElement} videoElement - The video element to play.
+ *
+ * @returns {Promise<void>}
+ */
+function playVideo(videoElement) {
+  const playPromise = videoElement.play();
+  if (playPromise !== undefined) {
+    playPromise.then(() => videoElement.play().then((r) => r).catch(() => null));
+  }
+}
+
+/**
+ * Sets up a video control with a play button, handling clicks and video element events.
+ *
+ * @param {HTMLElement} playButtonElement - The play button element.
+ * @param {HTMLVideoElement} videoElement - The video element to control.
+ *
+ * @returns {void}
+ */
+function setupVideoControl(playButtonElement, videoElement, videoLength) {
+  const totalFrames = 259.18;
+  const playProgressCircle = playButtonElement.querySelector('.play-progress-circle');
+  const progressCircle = playProgressCircle.querySelector('.progress-circle');
+  progressCircle.style.strokeDasharray = totalFrames;
+  progressCircle.style.strokeDashoffset = totalFrames;
+  const isReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
+
+  function togglePlayButton(element) {
+    const videoContainer = element.closest('.block.hero');
+    const playButton = videoContainer.querySelector('.video-play-button');
+    if (element.paused) {
+      playButton.setAttribute('aria-label', 'pause animation');
+      playButton.classList.add('paused');
+    } else {
+      playButton.setAttribute('aria-label', 'play animation');
+      playButton.classList.remove('paused');
+    }
+  }
+
+  function updateDashOffset() {
+    const currentTime = (videoElement.currentTime / videoLength) * totalFrames;
+    progressCircle.style.strokeDashoffset = totalFrames - currentTime;
+
+    // Check if the video is a loop video
+    if (videoElement.closest('div.hero')?.classList.contains('loop-video')) {
+      if (currentTime >= totalFrames) {
+        videoElement.currentTime = 0; // Reset video to start
+        playVideo(videoElement); // Play the video again
+      }
+      requestAnimationFrame(updateDashOffset);
+    } else if (currentTime >= totalFrames) {
+      // Check if the video has ended, reset dash offset and play again
+      progressCircle.style.strokeDashoffset = totalFrames;
+    } else {
+      // Schedule the next update for smoother animation
+      requestAnimationFrame(updateDashOffset);
+    }
+    if (currentTime === totalFrames) {
+      progressCircle.style.strokeDashoffset = totalFrames;
+    }
+  }
+
+  if (!isReducedMotion.matches) {
+    playButtonElement.addEventListener('click', () => {
+      if (videoElement.paused) {
+        playVideo(videoElement);
+        togglePlayButton(videoElement);
+      } else {
+        videoElement.pause();
+        togglePlayButton(videoElement);
+      }
+      updateDashOffset();
+    });
+  }
+
+  videoElement.addEventListener('timeupdate', updateDashOffset);
+  videoElement.addEventListener('ended', () => { togglePlayButton(videoElement); });
+  videoElement.addEventListener('play', () => { togglePlayButton(videoElement); });
+  videoElement.addEventListener('pause', () => { togglePlayButton(videoElement); });
+}
+
+/**
+ * Binds a play button to a video element, setting up a video control to play
+ * or pause the video when clicked.
+ *
+ * @param {Element} videoContainer - A list of video container elements.
+ *
+ * @returns {void}
+ */
+function bindVideoElement(videoContainer) {
+  if (videoContainer) {
+    const videoElement = videoContainer.querySelector('video');
+    const playButton = videoContainer.querySelector('.video-play-button');
+
+    if (videoElement) {
+      videoElement.load();
+      videoElement.addEventListener('loadedmetadata', () => {
+        const videoLength = videoElement.duration;
+        setupVideoControl(playButton, videoElement, videoLength);
+      });
+    }
+  }
+}
 
 export default function decorate(block) {
   const newChildren = [...block.children]
@@ -67,6 +211,9 @@ export default function decorate(block) {
       block.append(foregroundDiv);
     } else {
       block.append(videoElement);
+      const videoBtn = getVideoBtn();
+      block.append(videoBtn);
+      bindVideoElement(block);
     }
   }
 
